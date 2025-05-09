b08420581b8bcf79c84271e6e426471b56643e4c
dagop12 <dago.poblete.caballero@gmail.com>
2025-05-09 14:21:10 +0200

    added results for 1 big turbine

--- Wind_prod.py
+++ Wind_prod.py
@@ -9,18 +9,18 @@ df = pd.read_csv("Datasets/weather_data.csv",sep=",",index_col=0,header=[0, 1])
 
 # Parse time and convert wind speed to float
 df.index = pd.to_datetime(df.index, format="%d.%m.%Y %H:%M", errors="coerce")
-df.dropna(subset=[('wind_speed', '50')],inplace=True)
+df.dropna(subset=[('wind_speed', '92')],inplace=True)
 df["wind_speed"] = df["wind_speed"].astype(float)
 
 
 df["pressure"] = 1013.25  # in hPa
 
 # Choose a turbine type (e.g., 'ENERCON E 53 800')
-turbine = WindTurbine(turbine_type='E-53/800',hub_height=50)
+turbine = WindTurbine(turbine_type='E-115/3200',hub_height=92)
 
 # 4. Use ModelChain to compute power output
 mc = ModelChain(turbine).run_model(df)
 
 # 5. Output
 df['power_output_kW'] = mc.power_output.values/1000  # Convert to kW
-df.to_csv("Datasets/wind_turbine_output.csv", index=True)
+df.to_csv("Datasets/wind_turbine_output_wind_park.csv", index=True)
